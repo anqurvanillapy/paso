28c8087fb3d42d643e7777a9e157a0e0ba6cdc85
AnqurVanillapy <anqurvanillapy@gmail.com>
2018-12-02 23:43:10 +0800

    [ lexer ] Parsec init

---
+++ src/Ttt/Parsec.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Ttt.Parsec where
+
+newtype Parser a = Parser
+  { parse :: String -> [(a, String)]
+  }
+
+runParser :: Parser a -> String -> a
+runParser p s =
+  case parse p s of
+    [(x, [])] -> x
+    [(_, xs)] -> error "entire stream not cunsumed"
+    _         -> error "parse error"
+
+item :: Parser Char
+item = Parser $ \case
+  []      -> []
+  (x:xs)  -> [(x, xs)]
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+bind p f = Parser $ \s ->
+  concatMap (\(a, s') -> parse (f a) s') (parse p s)
+
+unit :: a -> Parser a
+unit a = Parser $ \s -> [(a, s)]
